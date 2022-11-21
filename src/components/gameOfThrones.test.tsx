a91c99c04dddaf77293ab42c890698151e4c022d
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-21 16:32:44 +0000

    tests added

---
+++ src/components/gameOfThrones.test.tsx
@@ -0,0 +1,7 @@
+import { formatSeasonAndEpisode } from "./gameOfThrones";
+
+test("formatSeasonAndEpisode returns an added zero when needed", () => {
+  expect(formatSeasonAndEpisode(10, 12)).toBe("S10E12");
+  expect(formatSeasonAndEpisode(1, 2)).toBe("S01E02");
+  expect(formatSeasonAndEpisode(14, 4)).toBe("S14E04");
+});
