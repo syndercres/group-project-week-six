9df8928dc10183624f8a20822486f359915f4c8d
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-22 12:08:29 +0000

    added shows

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GameOfThrones from "./components/gameOfThrones";
+import DisplayShows from "./components/showDisplay";
 
 ReactDOM.render(
   <React.StrictMode>
     <GameOfThrones />
+    <DisplayShows />
   </React.StrictMode>,
   document.getElementById("root")
 );
